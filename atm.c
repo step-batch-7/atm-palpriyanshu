0bc606b448b54ad7e96efa04d3ba0b3aa1de1f11
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-04-08 11:32:26 +0530

    modified print_denomination function

--- atm.c
+++ atm.c
@@ -3,23 +3,22 @@
 
 void print_denominations(unsigned short int money)
 {
-  unsigned int notes = get_money(money);
+  unsigned short int amount = money;
   int denomination[8] = {2000, 500, 100, 50, 20, 10, 5, 1} ;
   for(int count =0;count < 8 ; count++){
-    if(notes >> 28){
-      printf("%u notes of %d\n",notes >> 28 ,denomination[count]);
-    }
-    notes = notes << sizeof(int);
+    unsigned int notes = amount / denomination[count] ;
+    amount = amount % denomination[count];
+    notes && printf("%u %s of Rs %d\n",notes ,notes == 1 ? "note" : "notes", denomination[count]);
   }
 }
 
 unsigned int get_money(unsigned short int money)
 {
   unsigned short int amount = money;
-  unsigned int notes =0,total_notes = 0;
+  unsigned int notes =0,total_notes = 0x00000000;
   int denomination[8] = {2000, 500, 100, 50, 20, 10, 5, 1} ;
-  int bit_to_shift = 28;
-  if(amount/2000 >15){
+  int bit_to_shift = 28, max_allowed_amount = 31999;
+  if(amount < 1 || amount > 31999){
     return 0;
   }
 
