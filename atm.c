abf0dad2ae0a687fb1803592649825bf4398b9dd
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-04-08 11:01:55 +0530

    added print_denomination to print no of notes for each denomination

--- atm.c
+++ atm.c
@@ -1,27 +1,33 @@
+#include <stdio.h>
 #include "atm.h"
 
-unsigned short int deduct_amount(unsigned short int, int, unsigned int); 
-
-unsigned short int deduct_amount(unsigned short int amount, int denomination, unsigned int notes)
+void print_denominations(unsigned short int money)
 {
-   return amount - denomination * notes;
-};
+  unsigned int notes = get_money(money);
+  int denomination[8] = {2000, 500, 100, 50, 20, 10, 5, 1} ;
+  for(int count =0;count < 8 ; count++){
+    printf("%u notes of %d\n",notes >> 28 ,denomination[count]);
+    notes = notes << sizeof(int);
+  }
+}
 
 unsigned int get_money(unsigned short int money)
 {
   unsigned short int amount = money;
   unsigned int notes =0,total_notes = 0;
   int denomination[8] = {2000, 500, 100, 50, 20, 10, 5, 1} ;
-  int bit_to_rotate = 28;
+  int bit_to_shift = 28;
+  if(amount/2000 >15){
+    return 0;
+  }
 
-  for(int count =0; count < 8; count++){
-     notes = amount /denomination[count] ;
-     if(notes > 0 && notes < 16){
-      total_notes = (total_notes >> bit_to_rotate) | notes ;
-      total_notes = (total_notes << bit_to_rotate);
-     }
-    bit_to_rotate = bit_to_rotate - 4;
-    amount = deduct_amount(amount, denomination[count], notes);
+  for(int count =0; count < 8; count++)
+  {
+    notes = amount / denomination[count] ;
+    amount = amount % denomination[count];
+    total_notes = (total_notes >> bit_to_shift) | notes ;
+    total_notes = (total_notes << bit_to_shift);
+    bit_to_shift = bit_to_shift - 4;
   }
   return total_notes;
 };
